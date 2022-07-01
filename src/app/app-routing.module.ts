79387dc83c7f776b6c80a066fe7120f20fce5586
oury <barryoury64@gmail.com>
2022-07-01 00:17:00 +0000

    Added main pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './ui/pages/auth/login/login.component';
+import { RegisterComponent } from './ui/pages/auth/register/register.component';
+import { HomeComponent } from './ui/pages/home/home.component';
+import { NotFoundComponent } from './ui/pages/not-found/not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './ui/pages/auth/login/login.component';
+import { RegisterComponent } from './ui/pages/auth/register/register.component';
+import { NotFoundComponent } from './ui/pages/not-found/not-found.component';
+import { TextInputComponent } from './ui/components/forms/text-input/text-input.component';
+import { SubmitButtonComponent } from './ui/components/forms/submit-button/submit-button.component';
+import { ButtonComponent } from './ui/components/forms/button/button.component';
+import { HomeComponent } from './ui/pages/home/home.component';
+import { FolderComponent } from './ui/components/folder/folder.component';
+import { FileComponent } from './ui/components/file/file.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    TextInputComponent,
+    SubmitButtonComponent,
+    ButtonComponent,
+    HomeComponent,
+    FolderComponent,
+    FileComponent
   ],
   imports: [
     BrowserModule,
